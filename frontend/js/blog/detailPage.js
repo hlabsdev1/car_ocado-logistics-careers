1e24c494396f2739d4070cabfc408db70d7cda97
Suraj <yme9833@gmail.com>
2026-01-07 23:36:37 +0530

    blog detail added

---
+++ frontend/js/blog/detailPage.js
@@ -0,0 +1,15 @@
+function blogDetail() {
+  const section = document.querySelector('.blog-section');
+  const blogWrap = section.querySelector('.blog-wrap');
+  const emptyWrap = section?.querySelector('.w-dyn-empty');
+
+  if (emptyWrap) {
+    section.style.display = 'none';
+  }
+}
+
+var Webflow = Webflow || [];
+
+Webflow.push(function () {
+  blogDetail();
+});
